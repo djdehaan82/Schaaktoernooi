66e7d07b4e8f19ca14b54eb236c1c6fe87cb36d4
djdehaan82 <dennis.de.haan@capgemini.com>
2019-02-14 16:29:31 +0100

    final, almost there..

--- src/main/java/com/capgemini/Schaaktoernooi/SchaaktournooiApplication.java
+++ src/main/java/com/capgemini/Schaaktoernooi/SchaaktournooiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SchaaktoernooiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SchaaktournooiApplication.class, args);
+		SpringApplication.run(SchaaktoernooiApplicationtoernooiApplication.class, args);
 	}
 
 }
