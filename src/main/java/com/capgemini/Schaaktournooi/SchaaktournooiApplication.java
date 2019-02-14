b4f2374503a7babe0eb82c6b9d9cabe4be95c1f9
djdehaan82 <dennis.de.haan@capgemini.com>
2019-02-14 13:12:09 +0100

    first commit

---
+++ src/main/java/com/capgemini/Schaaktournooi/SchaaktournooiApplication.java
@@ -0,0 +1,14 @@
+package com.capgemini.Schaaktournooi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchaaktournooiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchaaktournooiApplication.class, args);
+	}
+
+}
+
